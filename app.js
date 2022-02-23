9f8d02d1a49aeb2eb27979a6a3a9dc386ee5f5c5
Automated Online Marketing Square Ventures <94171795+SQVentures21@users.noreply.github.com>
2022-02-23 17:58:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+//navbar
+
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
+
 //testimonial-slider
 var slideIndex = 1;
 showSlides(slideIndex);
@@ -45,4 +54,76 @@ for (i = 0; i < acc.length; i++) {
       panel.style.maxHeight = panel.scrollHeight + "px";
     }
   });
+}
+
+//css progressbar
+
+$(function () {
+  // Remove svg.radial-progress .complete inline styling
+  $("svg.radial-progress").each(function (index, value) {
+    $(this).find($("circle.complete")).removeAttr("style");
+  });
+
+  // Activate progress animation on scroll
+  $(window)
+    .scroll(function () {
+      $("svg.radial-progress").each(function (index, value) {
+        // If svg.radial-progress is approximately 25% vertically into the window when scrolling from the top or the bottom
+        if (
+          $(window).scrollTop() >
+            $(this).offset().top - $(window).height() * 0.75 &&
+          $(window).scrollTop() <
+            $(this).offset().top + $(this).height() - $(window).height() * 0.25
+        ) {
+          // Get percentage of progress
+          percent = $(value).data("percentage");
+          // Get radius of the svg's circle.complete
+          radius = $(this).find($("circle.complete")).attr("r");
+          // Get circumference (2πr)
+          circumference = 2 * Math.PI * radius;
+          // Get stroke-dashoffset value based on the percentage of the circumference
+          strokeDashOffset = circumference - (percent * circumference) / 100;
+          // Transition progress for 1.25 seconds
+          $(this)
+            .find($("circle.complete"))
+            .animate({ "stroke-dashoffset": strokeDashOffset }, 1250);
+        }
+      });
+    })
+    .trigger("scroll");
+});
+
+// starts evaluation
+
+$(".btn.one").mouseenter(function () {
+  $(".evaluare-text").text("Excelent");
+});
+$(".btn.two").mouseenter(function () {
+  $(".evaluare-text").text("Foarte Bun");
+});
+$(".btn.three").mouseenter(function () {
+  $(".evaluare-text").text("Bun");
+});
+$(".btn.four").mouseenter(function () {
+  $(".evaluare-text").text("Decent");
+});
+$(".btn.five").mouseenter(function () {
+  $(".evaluare-text").text("Slab");
+});
+
+$(".rating").mouseleave(function () {
+  $(".evaluare-text").text("Lasă o notă!");
+});
+
+//end stars
+
+//arrow div hide/show
+
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
