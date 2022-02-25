121b24ef0428c293c3849d045c646678753b4bbe
Automated Online Marketing Square Ventures <94171795+SQVentures21@users.noreply.github.com>
2022-02-25 15:57:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -308,3 +308,23 @@ btn12.onclick = function () {
     btn12.style.transform = "rotate(360deg)";
   }
 };
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
